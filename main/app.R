59cf811a66614affbd50b66ecd70fbafb5ab068e
pierre balaye <pierre@balaye.fr>
2023-08-19 10:46:59 +0000

    Use cookie to save state between sessions

--- main/app.R
+++ main/app.R
@@ -3,54 +3,81 @@ library(shiny)
 library(shinydashboard)
 library(main)
 library(ovalide)
+library(cookies)
 
-ui <- dashboardPage(
-  dashboardHeader(title = "DILAVO"),
-  
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Récap. score", tabName = "scores"),
-      selectInput("champ",  "Champ",  c(MCO = "mco",
-                                        SSR = "ssr",
-                                        HAD = "had",
-                                        PSY = "psy")),
-      selectInput("statut", "Statut", c(DGF = "dgf",
-                                        OQN = "oqn")),
-      hr(),
-      menuItem("MàJ. données", tabName = "update"),
-      selectInput("data", "Données", c("Scores",
-                                       "Tables",
-                                       "Contacts")),
-      hr()
-    )
-  ),
-  dashboardBody(
-    tags$head( 
-      tags$style(HTML("[data-toggle] { font-size: 24px; }"))
+ui <-
+  dashboardPage(
+    dashboardHeader(title = "DILAVO"),
+    
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Récap. score", tabName = "scores"),
+        selectInput("champ",  "Champ",  c(MCO = "mco",
+                                          SSR = "ssr",
+                                          HAD = "had",
+                                          PSY = "psy")),
+        selectInput("statut", "Statut", c(DGF = "dgf",
+                                          OQN = "oqn")),
+        hr(),
+        menuItem("MàJ. données", tabName = "update"),
+        selectInput("data", "Données", c("Scores",
+                                         "Tables",
+                                         "Contacts")),
+        hr()
+      )
     ),
-    tabItems(
-      tabItem(tabName = "scores", scoreTabSetUI("tabset")),
-      tabItem(tabName = "update", dataUploaderUI("uploader"))
+    dashboardBody(
+      tags$head( 
+        tags$style(HTML("[data-toggle] { font-size: 24px; }"))
+      ),
+      tabItems(
+        tabItem(tabName = "scores", scoreTabSetUI("tabset")),
+        tabItem(tabName = "update", dataUploaderUI("uploader"))
+      )
     )
-  )
-)
+  ) |> add_cookie_handlers()
 
 server <- function(input, output, session) {
-  last_state_file <- "last_state.rds"
   
-  if ( fs::file_exists(last_state_file) ) {
-    last_state <- readr::read_rds(last_state_file)
-    shiny::updateSelectInput(session, "champ", selected = last_state$champ)
-    shiny::updateSelectInput(session, "statut", selected = last_state$statut)
-    shiny::updateSelectInput(session, "data", selected = last_state$data)
-  }
-  
-  observeEvent(c(input$champ, input$statut, input$data), {
-    readr::write_rds(list( champ = input$champ,
-                           statut = input$statut,
-                           data   = input$data),
-                     last_state_file)
+  observeEvent( input$champ, {
+    set_cookie_response(
+      cookie_name = "champ",
+      cookie_value = input$champ
+    )
   })
+  observeEvent( input$statut, {
+    set_cookie_response(
+      cookie_name = "statut",
+      cookie_value = input$statut
+    )
+  })
+  observeEvent( input$data, {
+    set_cookie_response(
+      cookie_name = "data",
+      cookie_value = input$data
+    )
+  })
+  observeEvent(
+    get_cookie("champ"),
+    updateSelectInput(session,
+                      "champ",
+                      selected = get_cookie("champ")),
+    once = TRUE
+  )
+  observeEvent(
+    get_cookie("statut"),
+    updateSelectInput(session,
+                      "statut",
+                      selected = get_cookie("statut")),
+    once = TRUE
+  )
+  observeEvent(
+    get_cookie("data"),
+    updateSelectInput(session,
+                      "data",
+                      selected = get_cookie("data")),
+    once = TRUE
+  )
   
   
   scoreTabSetServer("tabset", reactive(nature(input$champ, input$statut)))
@@ -58,8 +85,7 @@ server <- function(input, output, session) {
                      reactive(input$champ),
                      reactive(input$statut),
                      reactive(input$data))
-  
-  
 }
 
-shinyApp(ui, server)
+shinyApp(ui, server,
+         options = list( launch.browser = TRUE))
